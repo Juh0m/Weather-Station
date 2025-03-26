d3da221cba1c050a4246c40168c5b6832ce7782d
Juho Maijala <juho.maijala@tuni.fi>
2025-03-26 18:20:36 +0200

    ISR, clean up code
    ISR should now work
    
    Also cleaned and fixed code and added a lot of comments

--- WeatherStation/WeatherStation.ino
+++ WeatherStation/WeatherStation.ino
@@ -1,58 +1,109 @@
+#include <Ethernet.h>
 #include <LiquidCrystal.h>
+#include <PubSubClient.h>
 #include <TimerOne.h>
+
 int lightSensorPin = A6;   // Pin for light sensor
-// int pin2 = 2; // Only pins 2 and 3 usable for ISR TODO
-// TODO: Change pin 2/3 as they are needed for ISR
-// RS pin = Arduino digital 2
-// Enable pin = Arduino digital 3
-// d4-d7 = Arduino digital pins 4-7
-const int rs = 2, en = 3, d7 = 4, d6 = 5, d5 = 6, d4 = 7;
+int isrPin = 2; // Pin for ISR (Digital signal). Must be either pin 2 or 3 on a nano.
+
+// RS pin = Arduino digital 3
+// Enable pin = Arduino digital 4
+// d4-d7 = Arduino digital pins 5-8
+const int rs = 3, en = 4, d4 = 8, d5 = 7, d6 = 6, d7 = 5;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
+void ISR();
+void timer_routine();
+
+// For pin_ISR
+int puls = 0;
+// Digital signal frequency
+float frequency;
+// For timer_routine
+volatile byte time = 0;
+
 void setup() {
+  // ISR pin
+  pinMode(isrPin, INPUT);
+  // Interrupt Service Routine for digital signal
+  // Every time the signal falls from 5V to 0V pin_ISR is called and puls increments by one.
+  // Every 5 seconds the frequency of the signal is calculated from puls.
+  // According to project requirements, relative humidity of 40% is 7.9kHz and relative humidity of 100% is 6.9kHz
+
+  // TODO: What frequency is 0% humidity? Does a 1% increase in humidity always increase frequency by the same amount?
+  // If that is the case, a 1% increase in humidity corresponds to a (-1/60*100)kHz decrease in frequency.
+  // 0% humidity would then be 8.567kHz (8.56666...)
+  // Humidity can then be calculated using the formula:
+  // RH = (0% RHf - frequency) * 60 -> (8.567 - f)*60
+
+  attachInterrupt(digitalPinToInterrupt(isrPin), ISR, FALLING); // Pin 2, Routine: pin_ISR, falling Edge
+
+  // Timer for ISR
+  // timerRoutine is called once every 1 second (1 000 000 uS).
+  Timer1.initialize(1000000);
+  Timer1.attachInterrupt(timerRoutine);
+
+  // Begin serial data transmission at 9600 baud (for printing values to the serial monitor)
   Serial.begin(9600);
-  //pinMode(pin2, INPUT);
+  // LCD begin, 20 positions, 2 rows (TODO: is 2 rows the max?)
+  // Set cursor at first position
   lcd.begin(20, 2);
   lcd.setCursor(0, 0);
-  createFinCharacters();
-  //attachInterrupt(digitalPinToInterrupt(pin2), pin_ISR, FALLING); // Pin 2, Routine: pin_ISR, falling Edge
+
+  // Letter Ä for LCD
+  createFinCharacter();
 }
 
 void loop() {
-  //Timer1.initialize(1000000) // 1 second (1 000 000 uS)
-  //Timer1.attachInterrupt(timer_routine)
-  double lightLevelVoltage = (analogRead(lightSensorPin) / 1024.0)*5.0; // 0-5V, 5V on 100% valoisuus
-  int lightLevelPercentage = (lightLevelVoltage / 5)*100; // En tiedä tarvitaanko
-  String voltageString = "JZNNITE ";
-  
-  voltageString.replace('Z', byte(1));
-  lcd.print((String)voltageString + String(lightLevelVoltage) + String("V"));
+  double lightLevelVoltage = (analogRead(lightSensorPin) / 1024.0)*5.0; // 0-5V, 0V is 0% light level, 5V = 100% light level
+  int lightLevelPercentage = (lightLevelVoltage / 5)*100; 
+
+  // Print voltage
+  // Only used for debug -> can be printed to only serial monitor, no need for lcd print
+  // Serial.println(lightLevelVoltage)
+
+  // Print on LCD (row 2)
   lcd.setCursor(0, 1);
-  lcd.print(String("VALOISUUS ") + String(lightLevelPercentage) + String("%"));
-  lcd.setCursor(0, 0);
+  lcd.print("VALOISUUS: ");
+  lcd.print(lightLevelPercentage);
+  lcd.print("%")
 
-
-  Serial.println(analogRead(lightSensorPin));
-  Serial.print(lightLevelVoltage);
-  Serial.println("V");
-  Serial.print(lightLevelPercentage);
-  Serial.println("% valoisuus");
+  // 300ms delay on loop() as updates to light level are not needed that often
+  // Could probably be increased to multiple seconds
   delay(300);
 }
 
-/*
-void pin_ISR // Interrupt service routine
+void ISR() // Interrupt service routine
 {
- // TODO
+  puls++;
 }
-void timer_routine
+void timerRoutine()
 {
-  // TODO
-}
-*/
+  time++;
 
-void createFinCharacters()
+  // Every 5 seconds calculate frequency and reset time
+  if(time > 4)
+  {
+    time = 0;
+    frequency = (float)puls/5.0;
+
+    Serial.print(puls);
+    puls = 0;
+    Serial.println("<- puls");
+    Serial.print(frequency);
+    Serial.println("<- taajuus");
+
+    // Print on LCD (row 1)
+    lcd.setCursor(0, 0);
+    lcd.print("TAAJUUS: ");  
+    lcd.print(frequency, 2);  
+    lcd.print(" Hz"); 
+}
+
+void createFinCharacter()
 {
+  // Letter Ä
+  // Usable with byte(1)
   byte CapitalAwithDots[8] = {
   B10001,
   B01110,
