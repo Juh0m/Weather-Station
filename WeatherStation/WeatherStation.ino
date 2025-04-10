dc9c7ab0921dd1602f539dd86f04e0c1998f3b17
Juho Maijala <juho.maijala@tuni.fi>
2025-04-10 20:39:21 +0300

    MQTT
    Lähettää MQTT- brokerille luetun säädatan

--- WeatherStation/WeatherStation.ino
+++ WeatherStation/WeatherStation.ino
@@ -3,6 +3,25 @@
 #include <PubSubClient.h>
 #include <TimerOne.h>
 
+byte server[] = {10,6,0,23 }; // MQTT- server IP-address
+unsigned int port = 1883; // MQTT- server port
+EthernetClient ethClient;
+void callback(char* topic, byte* payload, unsigned int length);
+
+PubSubClient client(server, port, callback, ethClient); 
+// A8 61 0A AE 59 C3
+static uint8_t macAddress[6] {0xA8, 0x61, 0x0A, 0xAE, 0x59, 0xC3}; 
+// Device ID 
+char* deviceId = "wysi727";
+// Client ID
+char* clientId = "a7272727w"; 
+char* deviceSecret = "oq2if";
+
+// Topics
+#define outTopic "ICT4_out_2020"
+#define outTopicLight "w727_valoisuus"
+#define outTopicMoisture "w727_kosteusIn"
+
 int lightSensorPin = A6;   // Pin for light sensor
 int isrPin = 2; // Pin for ISR (Digital signal). Must be either pin 2 or 3 on a nano.
 
@@ -12,15 +31,22 @@ int isrPin = 2; // Pin for ISR (Digital signal). Must be either pin 2 or 3 on a 
 const int rs = 3, en = 4, d4 = 8, d5 = 7, d6 = 6, d7 = 5;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
+
+
 void ISR_D();
 void timer_routine();
+void fetchIP();
+void connectMQTTServer();
 
 // For pin_ISR
 int puls = 0;
 // Digital signal frequency
 float frequency;
+float moisture10s;
+float lightLevel10s;
 // For timer_routine
 volatile byte time = 0;
+volatile byte time2 = 0;
 
 void setup() {
   // ISR pin
@@ -49,58 +75,126 @@ void setup() {
   lcd.begin(20, 2);
   lcd.setCursor(0, 0);
 
+  fetchIP();
+  connectMQTTServer();
   // Letter Ä for LCD
   createFinCharacter();
 }
 
 void loop() {
-  double lightLevelVoltage = (analogRead(lightSensorPin) / 1024.0)*5.0; // 0-5V, 0V is 0% light level, 5V = 100% light level
-  int lightLevelPercentage = (lightLevelVoltage / 5)*100; 
-
-  // Print voltage
-  // Only used for debug -> can be printed to only serial monitor, no need for lcd print
-  // Serial.println(lightLevelVoltage)
-
-  // Print on LCD (row 2)
-  lcd.setCursor(0, 1);
-  lcd.print("VALOISUUS: ");
-  lcd.print(lightLevelPercentage);
-  lcd.print("%");
-
-  // 600ms delay on loop() as updates to light level are not needed that often
-  // Could be increased to multiple seconds
-  delay(600);
+  // Nothing is done in the loop as everything is done with interrupts
 }
 
 void ISR_D() // Interrupt service routine
 {
-  puls++;
+  // Increment by one every time voltage falls
+  puls++; 
 }
 void timerRoutine()
 {
   time++;
 
-  // Every 5 seconds calculate frequency and reset time
-  if(time > 2)
+  // Every 2 seconds calculate frequency and reset time
+  if(time > 1)
   {
     time = 0;
-    frequency = (float)puls/3.0;
+    frequency = (float)puls/2.0;
 
-    Serial.print(puls);
+    // Light level
+    float lightLevelVoltage = (analogRead(lightSensorPin) / 1024.0)*5.0; // 0-5V, 0V is 0% light level, 5V = 100% light level
+    int lightLevelPercentage = (lightLevelVoltage / 5)*100; 
+    lightLevel10s += lightLevelPercentage;
+    // Print on LCD (row 2)
+    lcd.setCursor(0, 1);
+    lcd.print("VALOISUUS: ");
+    lcd.print(lightLevelPercentage);
+    lcd.print("%");
+
     puls = 0;
-    Serial.println("<- puls");
-    Serial.print(frequency);
-    Serial.println("<- taajuus"); 
 
-// If assumptions about signal are correct, this should work
+    // Moisture (in)
     float moisturePercentage = ((8567.0/1000.0) - (frequency/1000.0)) * 60.0;
+    moisture10s += moisturePercentage;
     // Print on LCD (row 1)
     lcd.setCursor(0, 0);
     lcd.print("ILMANKOSTEUS: ");  
-    lcd.print(moisturePercentage, 2);  
+    lcd.print(moisturePercentage, 1);  
     lcd.print("%"); 
 
+    time2++;
   }
+
+  // Every 10 seconds calculate average values of that time frame and send them to the broker
+  if(time2 > 4)
+  {
+    moisture10s = moisture10s/5.0;
+    lightLevel10s = lightLevel10s/5.0;
+    sendMQTTMessage();
+
+    // Reset values
+    time2 = 0;
+    moisture10s = 0;
+    lightLevel10s = 0;
+  }
+
+}
+void connectMQTTServer()
+{
+  if(client.connected())
+  {
+    Serial.println("Already connected.");
+    return;
+  }
+    Serial.println("Connecting to MQTT broker");
+    if (client.connect(clientId, deviceId, deviceSecret)) {
+        Serial.println("Connected successfully!");
+    } else {
+        Serial.println("Connection to broker failed."); 
+    }    
+}
+void sendMQTTMessage()
+{
+  char lightLevel[3];
+  char moisture[6];
+  dtostrf(moisture10s, 3, 0, moisture);
+  dtostrf(lightLevel10s, 2, 0, lightLevel);
+  char buffer[100];
+
+  // Does not work currently as the MQTT broker code is not working
+  //sprintf(buffer, "IOTJS={\"%s\":\"%s\",\"%s\":\"%s\",\"%s\":\"%s\",\"%s\":\"%s\"}","S_name1", outTopicLight, "S_value1", lightLevel, "S_name2", outTopicMoisture, "S_value2", moisture);
+  
+  if (!client.connected()) { // Check connection to MQTT broker
+      connectMQTTServer(); // If not connected, connect
+  }
+  if (client.connected()) { // If connected
+      client.publish(outTopic, buffer); // Send message to broker
+      sprintf(buffer, "IOTJS={\"%s\":\"%s\",\"%s\":\"%s\"}", "S_name", outTopicLight, "S_value", lightLevel);
+      Serial.println(buffer);
+      sprintf(buffer, "IOTJS={\"%s\":\"%s\",\"%s\":\"%s\"}", "S_name", outTopicMoisture, "S_value", moisture);
+      client.publish(outTopic, buffer); // Send message to broker
+      Serial.println("Messages sent to MQTT broker."); 
+      Serial.println(buffer);
+  } else {
+      Serial.println("Failed to send message: not connected to MQTT broker.");
+  }
+}
+// Fetch IP
+void fetchIP()
+{
+  bool connectionSuccess = Ethernet.begin(macAddress);
+  if(!connectionSuccess)
+  {
+    Serial.println("Failed to fetch IP");
+  }
+  else
+  {
+    Serial.println(Ethernet.localIP());
+  }
+}
+// PSC Callback
+void callback(char* topic, byte* payload, unsigned int length)
+{
+  Serial.println(topic);
 }
 
 void createFinCharacter()
