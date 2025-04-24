1b0eaca9843563a16d334ab450a27e2cf8d22800
Juh0m <141812817+Juh0m@users.noreply.github.com>
2025-04-24 21:07:02 +0300

    Move minimum and maximum calculations to a function (#8)
    * Move minimum and maximum calculations to a function
    
    * Call updateMinMax()

--- WeatherStation/WeatherStation.ino
+++ WeatherStation/WeatherStation.ino
@@ -50,6 +50,7 @@ LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 void ShowIP();
 void ShowSignals();
 void ShowSignalMinMax();
+void updateMinMax();
 void ISR_R();
 void timer_routine();
 void fetchIP();
@@ -223,37 +224,16 @@ void timerRoutine()
     lightLevel10s += lightLevelPercentage; // Add to 10s average
     puls = 0;
 
-    // Check if light level value was lower than minimum or higher than maximum
-    if(lightLevelPercentage > maxLightLevelPercentage)
-    {
-      maxLightLevelPercentage = lightLevelPercentage;
-    }
-    if(lightLevelPercentage < minLightLevelPercentage)
-    {
-      minLightLevelPercentage = lightLevelPercentage;
-    }
-
     // Moisture (in)
     frequency = (float)puls/2.0; 
     moisturePercentage = ((8567.0/1000.0) - (frequency/1000.0)) * 60.0; // Convert frequency to percentage
-  
+    if(moisturePercentage > 100)
+    {
+      moisturePercentage = 100;
+    }
+    updateMinMax();
     moisture10s += moisturePercentage; // Add to 10s average
-
     time2++; // Increment time2 by one
-
- // Check if moisture value was lower than minimum or higher than maximum
-    if(moisturePercentage > maxMoisturePercentage)
-    {
-      // Moisture can't go above 100%
-      if(moisturePercentage <= 100)
-      {
-        maxMoisturePercentage = moisturePercentage;
-      }
-    }
-    if(moisturePercentage < minMoisturePercentage)
-    {
-      minMoisturePercentage = moisturePercentage;
-    }
   }
 
   // Every 10 seconds calculate average values of that time frame and send them to the broker
@@ -329,7 +309,28 @@ void callback(char* topic, byte* payload, unsigned int length)
 {
   Serial.println(topic);
 }
+void updateMinMax()
+{
+    // Check if signal value was lower than minimum or higher than maximum
+    // The percentages will always be positive and under 100% so no need for additional checks
+    if(lightLevelPercentage > maxLightLevelPercentage)
+    {
+      maxLightLevelPercentage = lightLevelPercentage;
+    }
+    if(lightLevelPercentage < minLightLevelPercentage)
+    {
+      minLightLevelPercentage = lightLevelPercentage;
+    }
 
+    if(moisturePercentage > maxMoisturePercentage)
+    {
+      maxMoisturePercentage = moisturePercentage;
+    }
+    if(moisturePercentage < minMoisturePercentage)
+    {
+      minMoisturePercentage = moisturePercentage;
+    }
+}
 void createFinCharacter()
 {
   // Letter Ä
